39f33d70fe42dbe1cb8ac18ce5e307775fc5b68e
Sergi Estall <siez@gft.com>
2023-03-29 11:21:05 +0000

    Añadido Objectmapper en test que comprueban json en lugar de texto plano.

--- src/test/java/com/gftraining/microserviceProduct/controllers/ProductControllerTest.java
+++ src/test/java/com/gftraining/microserviceProduct/controllers/ProductControllerTest.java
@@ -1,11 +1,9 @@
 package com.gftraining.microserviceProduct.controllers;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gftraining.microserviceProduct.model.CategoryEntity;
@@ -13,17 +11,14 @@ import com.gftraining.microserviceProduct.model.ProductDTO;
 import com.gftraining.microserviceProduct.model.ProductEntity;
 import com.gftraining.microserviceProduct.services.ProductService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.webjars.NotFoundException;
-
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 
 @WebMvcTest(ProductController.class)
@@ -44,7 +39,7 @@ class ProductControllerTest {
 
     @Test
     void testGetAll() throws Exception {
-        Mockito.when(productService.allProducts())
+        when(productService.allProducts())
                 .thenReturn(productList);
 
         mockmvc.perform(MockMvcRequestBuilders.get("/products/getAll"))
@@ -65,7 +60,7 @@ class ProductControllerTest {
     void addNewProduct() throws Exception {
         ProductEntity product = new ProductEntity(109L,"A", new CategoryEntity(1L, "Libros", 20),"B", 2, 25);
 
-        Mockito.when(productService.saveProduct(Mockito.any(ProductDTO.class))).thenReturn(product.getId());
+        when(productService.saveProduct(any(ProductDTO.class))).thenReturn(product.getId());
 
         mockmvc.perform(MockMvcRequestBuilders.post("/products/newProduct")
                         .content(asJsonString(product))
@@ -76,18 +71,15 @@ class ProductControllerTest {
 
     @Test
     void getProductById() throws Exception {
-
         when(productService.getProductById(1398L)).thenReturn(productEntity);
 
         mockmvc.perform(get("/products/{id}",1398L))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().json("{\"id\":1398,\"name\":\"Pelota\",\"category\":{\"id\":1,\"name\":\"Juguetes\",\"discount\":20}" +
-                        ",\"description\":\"pelota futbol\",\"price\":19.99,\"stock\":24}"));
+                .andExpect(content().json(asJsonString(productEntity)));
     }
 
     @Test
     void getProductById_ThrowsNotFound() throws Exception {
-
         when(productService.getProductById(anyLong())).thenReturn(null);
 
         mockmvc.perform(get("/products/{id}",2L))
@@ -96,13 +88,11 @@ class ProductControllerTest {
 
     @Test
     void getProductByName() throws Exception {
-
         when(productService.getProductByName("Pelota")).thenReturn(productEntity);
 
         mockmvc.perform(get("/products/getByName/{name}","Pelota"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().json("{\"id\":1398,\"name\":\"Pelota\",\"category\":{\"id\":1,\"name\":\"Juguetes\",\"discount\":20}" +
-                        ",\"description\":\"pelota futbol\",\"price\":19.99,\"stock\":24}"));
+                .andExpect(content().json(asJsonString(productEntity)));
     }
 
     @Test
