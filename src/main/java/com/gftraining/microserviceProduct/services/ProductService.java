ece2260aaca704b18b532ab7c6033d8279acd839
Mar Amorós López <mraz@gft.com>
2023-03-29 10:39:05 +0000

    upload products from json file + json file with products

--- src/main/java/com/gftraining/microserviceProduct/services/ProductService.java
+++ src/main/java/com/gftraining/microserviceProduct/services/ProductService.java
@@ -1,12 +1,16 @@
 package com.gftraining.microserviceProduct.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gftraining.microserviceProduct.model.ProductDTO;
 import com.gftraining.microserviceProduct.model.ProductEntity;
 import com.gftraining.microserviceProduct.repositories.ProductRepository;
 import lombok.NonNull;
 import org.springframework.stereotype.Service;
-
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
 
 @Service
 public class ProductService {
@@ -39,4 +43,13 @@ public class ProductService {
 
         return productRepository.save(product).getId();
     }
+    public void updateProductsFromJson() throws IOException {
+       productRepository.deleteAll();
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<ProductEntity> products = objectMapper.readValue(new File("src/main/resources/data.json"), new TypeReference<List<ProductEntity>>(){});
+        productRepository.saveAll(products);
+
+    }
+
+
 }
--- src/test/java/com/gftraining/microserviceProduct/controllers/ProductControllerTest.java
+++ src/test/java/com/gftraining/microserviceProduct/controllers/ProductControllerTest.java
@@ -1,20 +1,15 @@
 package com.gftraining.microserviceProduct.controllers;
 
-import static org.hamcrest.Matchers.containsString;
+
 import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gftraining.microserviceProduct.model.CategoryEntity;
 import com.gftraining.microserviceProduct.model.ProductDTO;
 import com.gftraining.microserviceProduct.model.ProductEntity;
 import com.gftraining.microserviceProduct.services.ProductService;
-import io.swagger.v3.core.util.Json;
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
-
-import org.mockito.Mock;
 
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +17,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigDecimal;
 
 import java.util.Arrays;
 import java.util.List;
@@ -42,8 +37,8 @@ class ProductControllerTest {
     ProductService productService;
 
     List<ProductEntity> productList = Arrays.asList(
-            new ProductEntity(1L, "Playmobil", new CategoryEntity(1L, "Juguetes", 20), "juguetes de plástico", 40.00, 100)
-            , new ProductEntity(2L, "Espaguetis", new CategoryEntity(4L, "Comida", 25), "pasta italiana elaborada con harina de grano duro y agua", 2.00, 220)
+            new ProductEntity(1L, "Playmobil", new CategoryEntity(1L, "Juguetes", 20), "juguetes de plástico", new BigDecimal(40.00), 100)
+            , new ProductEntity(2L, "Espaguetis", new CategoryEntity(4L, "Comida", 25), "pasta italiana elaborada con harina de grano duro y agua", new BigDecimal(2.00), 220)
     );
 
     @Test
@@ -59,7 +54,7 @@ class ProductControllerTest {
     @Test
     void deleteProductById() throws Exception {
 
-        mockmvc.perform(MockMvcRequestBuilders.delete("/products/deleteById/{id}",1l))
+        mockmvc.perform(MockMvcRequestBuilders.delete("/products/{id}",1l))
                                                 .andExpect(MockMvcResultMatchers.status().isNoContent());
 
         verify(productService,times(1)).deleteProductById(anyLong());
@@ -67,7 +62,7 @@ class ProductControllerTest {
 
     @Test
     void addNewProduct() throws Exception {
-        ProductEntity product = new ProductEntity(109L,"A", new CategoryEntity(1L, "Libros", 20),"B", 2, 25);
+        ProductEntity product = new ProductEntity(109L,"A", new CategoryEntity(1L, "Libros", 20),"B", new BigDecimal(2), 25);
 
         Mockito.when(productService.saveProduct(Mockito.any(ProductDTO.class))).thenReturn(product.getId());
 
@@ -82,7 +77,7 @@ class ProductControllerTest {
     void getProductById_Test() throws Exception {
 
         ProductEntity productEntity = new ProductEntity(1398L,"Pelota",
-                new CategoryEntity(1L,"Juguetes",20),"pelota futbol",19.99,24);
+                new CategoryEntity(1L,"Juguetes",20),"pelota futbol",new BigDecimal(19.99),24);
 
         when(productService.getProductById(anyLong())).thenReturn(productEntity);
 
@@ -90,6 +85,15 @@ class ProductControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(content().json("{\"id\":1398,\"name\":\"Pelota\",\"category\":{\"id\":1,\"name\":\"Juguetes\",\"discount\":20}" +
                         ",\"description\":\"pelota futbol\",\"price\":19.99,\"stock\":24}"));
+    }
+
+    @Test
+    void updateProductsFromJson() throws Exception {
+
+        mockmvc.perform(MockMvcRequestBuilders.post("/products/JSON_load"))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+        verify(productService,times(1)).updateProductsFromJson();
     }
 
 
