914feac5ceee6d877bece0b2ec6431cf0f9d1b88
Joel Martin <jlaf@gft.com>
2023-04-03 10:49:26 +0000

    Adding intregation tests

---
+++ src/test/java/com/gftraining/microservice_product/integration_tests/controllers/ProductControllerIT.java
@@ -0,0 +1,102 @@
+package com.gftraining.microservice_product.integration_tests.controllers;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gftraining.microservice_product.model.ProductDTO;
+import com.gftraining.microservice_product.model.ProductEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Sql({"/data-test.sql"})
+class ProductControllerIT {
+    @Autowired
+    private MockMvc mockmvc;
+
+    ProductDTO productEntity = new ProductDTO(new ProductEntity(4L, "Pelota",
+            "Juguetes","pelota de futbol",new BigDecimal(19.99),24));
+
+
+    @Test
+    void testGetAll() throws Exception {
+        mockmvc.perform(MockMvcRequestBuilders.get("/products/getAll"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", isA(ArrayList.class)))
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.*", hasSize(3)))
+                .andExpect(jsonPath("$[*].name", containsInAnyOrder("Wonder", "Los Surcos del Azar", "Pelota")));
+    }
+
+    @Test
+    void getProductById() throws Exception {
+        mockmvc.perform(get("/products/{id}",1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().json("{id: 1, name: Wonder, stock: 90}"));
+    }
+
+    @Test
+    void getProductByName() throws Exception {
+        mockmvc.perform(get("/products/name/{name}","Los Surcos del Azar"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().json("[{id:2, name: \"Los Surcos del Azar\", price: 24.89}]"));
+    }
+
+    @Test
+    void putProductById() throws Exception {
+        mockmvc.perform(put("/products/{id}",1).contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(productEntity)))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void deleteProductById() throws Exception {
+        mockmvc.perform(delete("/products/{id}",1))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void updateProductsFromJson() throws Exception {
+        //Put your own path
+        mockmvc.perform(MockMvcRequestBuilders.post("/products/JSON_load")
+                        .param("path", "C:\\Files\\data_test.json"))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void addNewProduct() throws Exception {
+        mockmvc.perform(MockMvcRequestBuilders.post("/products/newProduct")
+                        .content(asJsonString(productEntity))
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().json("4"));
+    }
+
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/com/gftraining/microservice_product/integration_tests/services/ProductServiceIT.java
@@ -0,0 +1,101 @@
+package com.gftraining.microservice_product.integration_tests.services;
+
+
+import com.gftraining.microservice_product.model.CategoryEntity;
+import com.gftraining.microservice_product.model.ProductDTO;
+import com.gftraining.microservice_product.model.ProductEntity;
+import com.gftraining.microservice_product.repositories.ProductRepository;
+import com.gftraining.microservice_product.services.ProductService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceIT {
+
+    @InjectMocks
+    ProductService service;
+    @Mock
+    ProductRepository repository;
+
+    List<ProductEntity> productList = Arrays.asList(
+            new ProductEntity(1L, "Playmobil", "Juguetes",  "juguetes de plástico", new BigDecimal(40.00), 100),
+            new ProductEntity(2L, "Espaguetis",  "Comida",  "pasta italiana elaborada con harina de grano duro y agua", new BigDecimal(20.00), 220)
+    );
+    List<ProductEntity> productListSameName = Arrays.asList(
+            new ProductEntity(1L, "Playmobil", "Juguetes", "juguetes de plástico", new BigDecimal(40.00), 100),
+            new ProductEntity(2L, "Playmobil", "Juguetes", "juguetes de plástico", new BigDecimal(40.00), 100)
+    );
+    ProductEntity productEntity = new ProductEntity(1398L,"Pelota", "Juguetes", "pelota futbol",new BigDecimal(19.99),24);
+
+    @Test
+    void testGetAll() {
+        when(repository.findAll()).thenReturn(productList);
+        assertThat(service.getAll()).isEqualTo(productList);
+    }
+
+    @Test
+    void deleteProductById() {
+       service.deleteProductById(1L);
+       verify(repository,times(1)).deleteById(anyLong());
+    }
+
+    @Test
+    void saveProduct() {
+        when(repository.save(productEntity)).thenReturn(productEntity);
+        Long id = repository.save(productEntity).getId();
+
+        assertEquals(1398L, id);
+    }
+
+    @Test
+    void getProductById() {
+        when(repository.findById(1398L)).thenReturn(Optional.of(productEntity));
+
+        assertThat(service.getProductById(1398L)).isEqualToComparingFieldByFieldRecursively(productEntity);
+    }
+
+    @Test
+    void getProductByName() {
+
+        when(repository.findAllByName("Playmobil")).thenReturn(productListSameName);
+        assertThat(service.getProductByName("Playmobil")).isEqualTo(productListSameName);
+    }
+
+    @Test
+    void updateDatabase() throws IOException {
+        service.updateProductsFromJson("C:\\Files\\data-test.json");
+
+        verify(repository,times(1)).deleteAll();
+        verify(repository,times(1)).saveAll(any());
+    }
+
+    @Test
+    void putProductById() {
+        when(repository.findById(anyLong())).thenReturn(Optional.of(productEntity));
+        service.putProductById(new ProductDTO(productEntity), 1L);
+        verify(repository,times(1)).findById(anyLong());
+        verify(repository,times(1)).save(any());
+    }
+
+    @Test
+    void calculateFinalPrice(){
+        BigDecimal realParam = new BigDecimal("23.85");
+        BigDecimal expectedParam = new BigDecimal("21.47");
+
+        assertThat(service.calculateFinalPrice(realParam, 10)).isEqualByComparingTo(expectedParam);
+    }
+}
