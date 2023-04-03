c51f7e63ccb95161f2b2d5d3668d0b68d16e3dc6
Mar Amorós López <mraz@gft.com>
2023-04-03 14:18:01 +0000

    cambios (aun no va)

--- src/main/java/com/gftraining/microservice_product/configuration/LiquibaseConfiguration.java
+++ src/main/java/com/gftraining/microservice_product/configuration/LiquibaseConfiguration.java
@@ -2,14 +2,19 @@ package com.gftraining.microservice_product.configuration;
 
 import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
 
+@Component
+@Configuration
 public class LiquibaseConfiguration {
     DataSourceConfiguration dataSource;
 
     public LiquibaseConfiguration(DataSourceConfiguration dataSource) {
         this.dataSource = dataSource;
     }
-
+    @DependsOn("entityManagerFactory")
     @Bean
     public SpringLiquibase liquibase() {
         SpringLiquibase liquibase = new SpringLiquibase();
