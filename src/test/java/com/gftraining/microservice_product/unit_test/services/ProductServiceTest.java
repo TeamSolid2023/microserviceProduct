a2a04d00d0abee1813c62a73b1b9469c1d374d89
Joel Martin <jlaf@gft.com>
2023-04-12 14:17:09 +0000

    Added DisplayName to ServiceTest

--- src/test/java/com/gftraining/microservice_product/unit_test/services/ProductServiceTest.java
+++ src/test/java/com/gftraining/microservice_product/unit_test/services/ProductServiceTest.java
@@ -20,7 +20,6 @@ import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
@@ -48,13 +47,42 @@ class ProductServiceTest {
     ProductEntity productEntity = new ProductEntity(1L,"Pelota", "Juguetes","pelota futbol",new BigDecimal(19.99),24);
 
     @Test
+    @DisplayName("When calling getAll, Then a list of Products is returned")
     void testGetAll() {
         given(repository.findAll()).willReturn(productList);
+
         assertThat(service.getAll()).isEqualTo(productList);
     }
 
     @Test
-    @DisplayName("given a product id, when delete product by id, then the product is deleted")
+    @DisplayName("Given a product id, When finding a product on the repository, Then the product is returned")
+    void getProductById() {
+        given(repository.findById(anyLong())).willReturn(Optional.of(productEntity));
+
+        assertThat(service.getProductById(1L)).isEqualToComparingFieldByFieldRecursively(productEntity);
+    }
+
+    @Test
+    @DisplayName("Given a product name, When finding products on the repository by name, Then a list of products with that name is returned")
+    void getProductByName() {
+        given(repository.findAllByName(anyString())).willReturn(productListSameName);
+
+        assertThat(service.getProductByName("Playmobil")).isEqualTo(productListSameName);
+    }
+
+    @Test
+    @DisplayName("Given a Product, When the product is saved, Then verify if repository is called and if the id is 1")
+    void putProductById() {
+        given(repository.save(productEntity)).willReturn(productEntity);
+
+        Long id = repository.save(productEntity).getId();
+
+        verify(repository).save(any());
+        assertThat(id).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("Given a product id, When delete product by id, Then the product is deleted")
     void deleteProductById() {
         //given
         given(repository.findById(anyLong())).willReturn(Optional.of(productEntity));
@@ -68,30 +96,18 @@ class ProductServiceTest {
     }
 
     @Test
+    @DisplayName("Given a product id, When finding a product on the repository, Then the product is returned")
     void saveProduct() {
         given(repository.save(productEntity)).willReturn(productEntity);
+
         Long id = repository.save(productEntity).getId();
+
         verify(repository).save(any());
-        assertEquals(1L, id);
+        assertThat(id).isEqualTo(1L);
     }
 
     @Test
-    @DisplayName("given a product id, when finding a product on the repository, then the product is returned")
-    void getProductById() {
-        given(repository.findById(anyLong())).willReturn(Optional.of(productEntity));
-
-        assertThat(service.getProductById(1L)).isEqualToComparingFieldByFieldRecursively(productEntity);
-    }
-
-    @Test
-    @DisplayName("given a product name, when finding products on the repository by name, then a list of products with that name is returned")
-    void getProductByName() {
-        given(repository.findAllByName(anyString())).willReturn(productListSameName);
-
-        assertThat(service.getProductByName("Playmobil")).isEqualTo(productListSameName);
-    }
-
-    @Test
+    @DisplayName("Given a path, When calling updateProductsFromJson, Then verify if repository is called")
     void updateDatabase() throws IOException {
         //Put your own path
         service.updateProductsFromJson("C:\\Files\\data.json");
@@ -101,18 +117,9 @@ class ProductServiceTest {
     }
 
     @Test
-    void putProductById() {
-        given(repository.save(productEntity)).willReturn(productEntity);
-        
-        Long id = repository.save(productEntity).getId();
-        
-        verify(repository).save(any());
-        assertThat(id).isEqualTo(1L);
-    }
-
-    @Test
+    @DisplayName("Given an id and an units, When calling updateStock, Then verify if repository is called")
     void updateStock() {
-        when(repository.findById(1L)).thenReturn(Optional.of(productEntity));
+        given(repository.findById(1L)).willReturn(Optional.of(productEntity));
 
         service.updateStock(100, 1L);
 
@@ -121,6 +128,7 @@ class ProductServiceTest {
     }
 
     @Test
+    @DisplayName("Given a BigDecimal and a discount, When calling calculateFinalPrice, Then the result has to be equal to expectedParam")
     void calculateFinalPrice(){
         BigDecimal realParam = new BigDecimal("23.85");
         BigDecimal expectedParam = new BigDecimal("21.47");
@@ -129,7 +137,9 @@ class ProductServiceTest {
     }
 
     @Test
+    @DisplayName("Given a Product, When getting the discount, Then discount has to be 0")
     void getDiscount(){
         assertThat(service.getDiscount(productEntity)).isZero();
     }
+
 }
